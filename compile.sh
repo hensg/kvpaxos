5621bdb197d69f42a1ee348b1ce0876c534fb45a
Henrique Goulart <henriquedsg89@gmail.com>
2022-12-10 01:19:33 -0300

    refact compile command for release

--- compile.sh
+++ compile.sh
@@ -1,13 +1,13 @@
-#git submodule update --init --recursive
-#git pull --recurse-submodules
-#cd deps/libpaxos
-#git checkout master
-# cd ../../
+git submodule update --init --recursive
+git pull --recurse-submodules
+cd deps/libpaxos
+git checkout master
+cd ../../
 
 mkdir -p build
 cd build
-export CC=/usr/bin/gcc
-export CXX=/usr/bin/g++
-#cmake ..
+#export CC=/usr/bin/gcc
+#export CXX=/usr/bin/g++
+#cmake -DCMAKE_BUILD_TYPE=Debug ..
 cmake -DCMAKE_BUILD_TYPE=Release ..
 make
